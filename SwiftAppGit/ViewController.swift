6ef0dcaaa9e404e53cdbb99a270dc96fecf4acd0
Iann221 <vincentiannugroho@gmail.com>
2022-04-07 08:54:22 +0700

    initial commit

---
+++ SwiftAppGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftAppGit
+//
+//  Created by Vincentius Ian Widi Nugroho on 07/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
